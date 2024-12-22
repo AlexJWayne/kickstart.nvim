3e312a38544e36468fa39891caf3e4a277adca08
AlexJWayne <alex.wayne@solvenergy.com>
2024-12-22 00:37:50 -0800

    Try some other themes

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,55 @@
+-- return {
+--   'rose-pine/neovim',
+--   name = 'rose-pine',
+--   config = function()
+--     vim.cmd 'colorscheme rose-pine'
+--   end,
+-- }
+
+return {
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  init = function()
+    vim.cmd.colorscheme 'tokyonight-night'
+  end,
+}
+
+-- return {
+--   'catppuccin/nvim',
+--   priority = 1000,
+--   name = 'catppuccin',
+--   init = function()
+--     vim.cmd.colorscheme 'catppuccin-mocha'
+--   end,
+-- }
+
+-- return {
+--   'sainnhe/gruvbox-material',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.gruvbox_material_enable_italic = true
+--     vim.cmd.colorscheme 'gruvbox-material'
+--   end,
+-- }
+
+-- return {
+--   'Mofiqul/vscode.nvim',
+--   priority = 1000,
+--
+--   config = function()
+--     require('vscode').setup {
+--       italic_comments = true,
+--       underline_links = true,
+--     }
+--   end,
+--
+--   init = function()
+--     vim.cmd.colorscheme 'vscode'
+--     vim.cmd.hi 'Comment gui=none' -- from kickstart ???
+--   end,
+-- }
